e68b4b9934a0ed952cd75ac3c3e70cdf1d771165
JoaoPedro53 <jpsv32@gmail.com>
2026-04-17 12:57:36 -0300

    feat: created new tests of the OrderController, listAll -> with argument name give is found and argument name is not found.

--- src/test/java/jota/developer/controller/OrderControllerTest.java
+++ src/test/java/jota/developer/controller/OrderControllerTest.java
@@ -6,17 +6,12 @@ import jota.developer.domain.School;
 import jota.developer.enums.StatusPayment;
 import jota.developer.enums.UniformSizeUp;
 import jota.developer.enums.UniformType;
-import jota.developer.mapper.OrderMapperImpl;
-import jota.developer.repository.OrderRepository;
 import jota.developer.repository.OrderRepositoryData;
-import jota.developer.service.OrderService;
 import org.junit.jupiter.api.*;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,7 +19,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.time.LocalDate;
@@ -59,22 +53,48 @@ class OrderControllerTest {
         var order2 = Order.builder().orderId(2L).moneyGiven(50.0).statusPayment(StatusPayment.PENDING_PAYMENT)
                 .deliveryDate(LocalDate.of(2026, 03, 20)).purchaseDate(dateTimeFormated)
                 .details("").quantity(1).school(new School("Livramento")).uniformType(UniformType.SHIRT)
-                .uniformSizeUp(UniformSizeUp.M).client(new Client("João", "82 99760-2347")).build();
+                .uniformSizeUp(UniformSizeUp.M).client(new Client("Pedro", "82 99760-2347")).build();
         var order3 = Order.builder().orderId(3L).moneyGiven(50.0).statusPayment(StatusPayment.PENDING_PAYMENT)
                 .deliveryDate(LocalDate.of(2026, 03, 20)).purchaseDate(dateTimeFormated)
                 .details("").quantity(1).school(new School("Livramento")).uniformType(UniformType.SHIRT)
-                .uniformSizeUp(UniformSizeUp.M).client(new Client("João", "82 99760-2347")).build();
+                .uniformSizeUp(UniformSizeUp.M).client(new Client("Alfredo", "82 99760-2347")).build();
         ordersList.addAll(List.of(order1, order2, order3));
     }
 
     @Test
-    @DisplayName("listAllOrdersOrOrdersByNameClient return all orders when argument name is null")
+    @DisplayName("GET v1/orders return all orders list when argument name is null")
     @org.junit.jupiter.api.Order(1)
     void listAllOrdersOrOrdersByNameClient_ReturnAllOrders_WhenArgumentIsNull() throws Exception {
         BDDMockito.when(repositoryData.getORDERS()).thenReturn(ordersList);
 
         var response = readResourceFile("order/get-order-null-name-200.json");
         mockMvc.perform(MockMvcRequestBuilders.get("/v1/orders"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(response));
+    }
+
+    @Test
+    @DisplayName("GET v1/orders?name=João return orders list by name client when argument name is give and is found")
+    @org.junit.jupiter.api.Order(2)
+    void listAllOrdersOrOrdersByNameClient_ReturnOrdersByNameClient_WhenArgumentNameIsGive() throws Exception {
+        BDDMockito.when(repositoryData.getORDERS()).thenReturn(ordersList);
+
+        var response = readResourceFile("order/get-order-by-name-joao-200.json");
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/orders").param("name", "João"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(response));
+    }
+
+    @Test
+    @DisplayName("GET v1/orders?name=x return orders empty list when client name give is not found")
+    @org.junit.jupiter.api.Order(3)
+    void listAllOrdersOrOrdersByNameClient_ReturnOrdersEmptyList_WhenArgumentClientNameGiveIsNotFound() throws Exception {
+        BDDMockito.when(repositoryData.getORDERS()).thenReturn(ordersList);
+
+        var response = readResourceFile("order/get-order-by-name-x-200.json");
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/orders").param("name", "x"))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().json(response));
