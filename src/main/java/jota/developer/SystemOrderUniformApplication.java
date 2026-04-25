2ae442b04bdcda545ef2f44761079d9ea556ed9e
JoaoPedro53 <jpsv32@gmail.com>
2026-04-25 00:36:25 -0300

    feat: added '@EnableConfigurationProperties' to scan the class ConnectionConfigurationProperties.

--- src/main/java/jota/developer/SystemOrderUniformApplication.java
+++ src/main/java/jota/developer/SystemOrderUniformApplication.java
@@ -1,13 +1,16 @@
 package jota.developer;
 
+import jota.developer.config.ConnectionConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ConnectionConfigurationProperties.class)
 public class SystemOrderUniformApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SystemOrderUniformApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SystemOrderUniformApplication.class, args);
+    }
 
 }
